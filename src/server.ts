12003728db636ced10b3344aa364cb14ce037a88
artur <arturdolzan@gmail.com>
2020-11-10 11:07:21 -0300

    Add .eslintignore file

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (request, response) => {
-
-  return response.json({message: 'Hello World'})
+  return response.json({ message: 'Hello World' })
 })
 
 app.listen(process.env.PORT || 3004)
